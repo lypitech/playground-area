7febf4908dee41e669f3379aa2d4e564e13019f0
Babouye <esteban.bouyault-yvanez@epitech.eu>
2025-09-16 13:53:27 +0200

    feat(front>test>svelte): Added home page with little router and low quality design

---
+++ frontend/svelte/svelte/src/routes/about/+page.svelte
@@ -0,0 +1,67 @@
+<section>
+
+    <section class="button">
+        <button on:click={() => history.back()}>Back</button>
+    </section>
+
+    <section class="title">
+        <h1>About</h1>
+        <div>
+            Know more about our project
+        </div>
+    </section>
+</section>
+
+<style>
+    h1 {
+        text-align: center;
+        color: white;
+        font-size: 4em;
+    }
+
+    div {
+        text-align: center;
+        font-size:center 1.5em;
+        color: white;
+    }
+
+    button {
+        font-size: 1em;
+        padding: 0.5em;
+        border-radius: 1em;
+        background-color: white;
+        color: blueviolet;
+        cursor: pointer;
+
+        &:hover {
+            background-color: lightyellow;
+            scale: 1.1;
+        }
+    }
+
+    section {
+        display: flex;
+        flex-direction: raw;
+        justify-content: center;
+        align-items: center;
+        background: blueviolet;
+
+    }
+
+    section.button {
+        place-self: flex-start;
+        background-color: blueviolet;
+        padding-top: 1em;
+        padding-left: 1em;
+    }
+
+    section.title {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 0.6;
+        background-color: blueviolet;
+        padding: 2em;
+    }
+</style>
---
+++ frontend/svelte/svelte/src/routes/workflows/+page.svelte
@@ -0,0 +1,54 @@
+<section class="button">
+    <button on:click={() => history.back()}>Back</button>
+</section>
+
+<section class="title">
+    <h1>Workflows</h1>
+    <div>
+        first workflows
+    </div>
+</section>
+
+<style>
+    h1 {
+        text-align: center;
+        color: white;
+        font-size: 4em;
+    }
+
+    div {
+        text-align: center;
+        font-size: 1.5em;
+        color: white;
+    }
+
+    button {
+        font-size: 1em;
+        padding: 0.5em;
+        border-radius: 1em;
+        background-color: white;
+        color: blueviolet;
+        cursor: pointer;
+
+        &:hover {
+            background-color: lightyellow;
+            scale: 1.1;
+        }
+    }
+
+    section.button {
+        background-color: blueviolet;
+        padding-top: 1em;
+        padding-left: 1em;
+    }
+
+    section.title {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 0.6;
+        background-color: blueviolet;
+        padding: 2em;
+    }
+</style>
